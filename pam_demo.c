dcd03ec3f1f06374dd49d7a7bdf9015b740fa574
wangli(李望) <wangli@tencent.com>
2017-04-05 18:30:37 +0800

    syslog

--- pam_demo.c
+++ pam_demo.c
@@ -1,4 +1,4 @@
-
+#include <syslog.h>
 #include <stdio.h>
 #include <security/pam_modules.h>
 
@@ -10,6 +10,8 @@ PAM_EXTERN int pam_sm_setcred( pam_handle_t *pamh, int flags, int argc, const ch
 }
 
 PAM_EXTERN int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, const char **argv ) {
+    setlogmask(LOG_DEBUG);
+    syslog(LOG_DEBUG, "wangli_pam_demo\n");
     int retval;
     char* pUsername;
     retval = pam_get_user(pamh, &pUsername, "TUsername: ");
