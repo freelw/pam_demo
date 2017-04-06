33fc9f84b8f48765e8908712d6e1fab1d7015e0f
李望 <liwang@192.168.9.113>
2017-04-06 20:08:35 +0800

    1

--- pam_demo.c
+++ pam_demo.c
@@ -18,16 +18,17 @@ PAM_EXTERN int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, cons
     const struct pam_conv *conv;
     struct pam_message resp_message;
     const struct pam_message *msg[1];
-    const struct pam_response *resp[1];
+    const struct pam_response resp[1];
+    const struct pam_response *presp = resp;
       
     resp_message.msg_style = PAM_PROMPT_ECHO_OFF;  
     resp_message.msg = "come on:";  
     msg[0] = &resp_message; 
     int retval = pam_get_item(pamh, PAM_CONV, (const void **)&conv);
     printf("retVal get_conv : %d\n", retval);
-    retval = conv->conv(1, msg, resp, conv->appdata_ptr);
+    retval = conv->conv(1, msg, &presp, conv->appdata_ptr);
     printf("retVal conv : %d\n", retval);
-    printf("resp->resp : %s\n", resp[0]->resp);
+    printf("resp->resp : %s\n", resp[0].resp);
     //free(resp);
     //const char* pUsername;
     //retval = pam_get_user(pamh, (const char**)&pUsername, "TUsername: ");
