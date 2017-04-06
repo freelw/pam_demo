ab7017e3e784e987b3cb259cc08b37613a10f09a
李望 <liwang@192.168.9.113>
2017-04-06 20:10:12 +0800

    1

--- pam_demo.c
+++ pam_demo.c
@@ -18,8 +18,8 @@ PAM_EXTERN int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, cons
     const struct pam_conv *conv;
     struct pam_message resp_message;
     const struct pam_message *msg[1];
-    const struct pam_response resp[1];
-    const struct pam_response *presp = resp;
+    struct pam_response resp[1];
+    struct pam_response *presp = resp;
       
     resp_message.msg_style = PAM_PROMPT_ECHO_OFF;  
     resp_message.msg = "come on:";  
