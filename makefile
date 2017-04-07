88c0976e5c0e119c724d2f04c2874e69076bd393
freelw <liwang112358@gmail.com>
2017-04-07 05:09:15 +0000

    makefile -Wl

--- makefile
+++ makefile
@@ -1,8 +1,13 @@
 TARGET:=pam_demo
 all:
-	g++ -fPIC -c $(TARGET).c
-	g++ -shared -o $(TARGET).so $(TARGET).o -lpam
+	g++ -fPIC -c $(TARGET).c  
+	#g++ -c -fPIC $(TARGET).c -o $(TARGET).o 
+#	g++ -c -fPIC /usr/local/lib/libqrencode.a  -o qren.o
+	#ld -shared -fPIC  $(TARGET).o /usr/local/lib/libqrencode.a -o merge.o
+	#g++ -c -fPIC merge0.o -o merge.o
+	#g++ -shared -o $(TARGET).so $(TARGET).o -lpam -lqrencode -Wl,--whole-archive /usr/local/lib/libqrencode.a 
+	g++ -shared -o $(TARGET).so -lpam -Wl,--whole-archive /usr/local/lib/libqrencode.a -Wl,--no-whole-archive
 cleanall:
-	rm $(TARGET).so $(TARGET).o
+	rm $(TARGET).so $(TARGET).o 
 install: all
 	cp $(TARGET).so /lib/x86_64-linux-gnu/security/$(TARGET).so
--- pam_demo.c
+++ pam_demo.c
@@ -10,29 +10,6 @@
 #include "qrencode.h"
 
 
-char sqrcode[1024*1024];
-int create_qr_code(const char *str)
-{
-    memset(sqrcode, 0, sizeof(sqrcode));
-    size_t index = 0;
-    QRcode *qrcode=QRcode_encodeString(str, 0, QR_ECLEVEL_H, QR_MODE_8, 1);
-    for (size_t i = 0; i < 25; ++ i) {
-        for (size_t j = 0; j < 25; ++ j) {
-            if(qrcode->data[i*25+j]&0x01) {
-                sqrcode[index++] = ' ';
-                printf("#");
-            } else {
-                char _[] = "\033[47m\n \033[0m";
-		strcpy(sqrcode+index, _);
-		index += strlen(_);
-                printf("_");
-            }
-            sqrcode[index++] = '\n';
-            printf("\n");
-        }
-    }
-    return 0;
-}
 
 PAM_EXTERN int pam_sm_setcred( pam_handle_t *pamh, int flags, int argc, const char **argv ) {
     printf("setcred\n");
@@ -46,9 +23,9 @@ PAM_EXTERN int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, cons
     const struct pam_message *msg[1];
     struct pam_response *presp;
     resp_message.msg_style = PAM_PROMPT_ECHO_OFF;  
-    create_qr_code("http://www.qq.com");
-    //resp_message.msg = "come on:\033[47m\n   \033[0m   \033[47m   \033[0m";  
-    resp_message.msg = sqrcode;
+    //create_qr_code("http://www.qq.com");
+    resp_message.msg = "come on:\033[47m\n   \033[0m   \033[47m   \033[0m";  
+    //resp_message.msg = sqrcode;
     msg[0] = &resp_message; 
     int retval = pam_get_item(pamh, PAM_CONV, (const void **)&conv);
     printf("retVal get_conv : %d\n", retval);
@@ -96,4 +73,27 @@ PAM_EXTERN int pam_sm_close_session(pam_handle_t *pamh,int flags,int argc, const
 {
     printf("closesession\n");
     return PAM_SUCCESS;
+}
+int create_qr_code(const char *str)
+{
+    static char sqrcode[1024*1024];
+    memset(sqrcode, 0, sizeof(sqrcode));
+    size_t index = 0;
+    QRcode *qrcode=QRcode_encodeString(str, 0, QR_ECLEVEL_H, QR_MODE_8, 1);
+ /*   for (size_t i = 0; i < 25; ++ i) {
+        for (size_t j = 0; j < 25; ++ j) {
+            if(qrcode->data[i*25+j]&0x01) {
+                sqrcode[index++] = ' ';
+                printf("#");
+            } else {
+                char _[] = "\033[47m\n \033[0m";
+		strcpy(sqrcode+index, _);
+		index += strlen(_);
+                printf("_");
+            }
+            sqrcode[index++] = '\n';
+            printf("\n");
+        }
+    }*/
+    return 0;
 }
