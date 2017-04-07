8262b2aaa37c17f05adad5a75baa7976547a9293
李望 <liwang@liwangdeMacBook-Pro.local>
2017-04-07 11:15:24 +0800

    1

--- pam_demo.c
+++ pam_demo.c
@@ -8,11 +8,26 @@
 #define PAM_SM_PASSWORD
 #include <security/pam_modules.h>
 #include "qrencode.h"
+#include <string>
 
 
-int create_qr_code(const char *str)
+int create_qr_code(const char *str, std::string & sqrcode)
 {
     QRcode *qrcode=QRcode_encodeString(str, 0, QR_ECLEVEL_H, QR_MODE_8, 1);
+    sqrcode = "";
+    for (size_t i = 0; i < 25; ++ i) {
+        for (size_t j = 0; j < 25; ++ j) {
+            if(qrcode->data[i*25+j]&0x01) {
+                sqrcode += " ";
+                printf("#");
+            } else {
+                sqrcode += "\033[47m\n \033[0m";
+                printf("_");
+            }
+            sqrcode += "\n";
+            printf("\n");
+        }
+    }
     return 0;
 }
 
@@ -27,9 +42,11 @@ PAM_EXTERN int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, cons
     struct pam_message resp_message;
     const struct pam_message *msg[1];
     struct pam_response *presp;
-      
     resp_message.msg_style = PAM_PROMPT_ECHO_OFF;  
-    resp_message.msg = "come on:\033[47m\n   \033[0m   \033[47m   \033[0m";  
+    string sqrcode;
+    create_qr_code("http://www.qq.com", sqrcode);
+    //resp_message.msg = "come on:\033[47m\n   \033[0m   \033[47m   \033[0m";  
+    resp_message.msg = sqrcode.c_str();
     msg[0] = &resp_message; 
     int retval = pam_get_item(pamh, PAM_CONV, (const void **)&conv);
     printf("retVal get_conv : %d\n", retval);
